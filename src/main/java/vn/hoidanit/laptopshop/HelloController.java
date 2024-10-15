6901e734a1f2eb3e31d43546439909f6f32a7a2a
MAC <mac@MacBook-Air-cua-MAC.local>
2024-10-16 00:29:44 +0700

    security truoc

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello world from with hoidanit !";
+        return "Hello world from with hoidanit 1432!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Hello world from with user!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Hello world from with admin!";
     }
 
 }
