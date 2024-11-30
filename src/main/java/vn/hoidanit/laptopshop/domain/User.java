f02f3155b0f04d891da95f556d28655361d6581c
bangnguyenthi <20211610@eaut.edu.vn>
2024-11-30 23:29:14 +0700

    database

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,11 +1,18 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 // kết nối vói csdl
 public class User {
 
@@ -19,7 +26,16 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
-   
+    private String avatar;
+    
+   // roles_id
+   //User many --> to one --> role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 
     //contructor
     
@@ -28,17 +44,19 @@ public class User {
     }
 
 
-    public User(long id, String email, String password, String fullName, String address, String phone) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.fullName = fullName;
-        this.address = address;
-        this.phone = phone;
-    }
+
+    public User(long id, String email, String password, String fullName, String address, String phone, String avatar) {
+            this.id = id;
+            this.email = email;
+            this.password = password;
+            this.fullName = fullName;
+            this.address = address;
+            this.phone = phone;
+            this.avatar = avatar;
+        }
 
 
-//getter setter
+    //getter setter
     public long getId() {
         return id;
     }
@@ -98,14 +116,25 @@ public class User {
         this.phone = phone;
     }
 
- //toString()
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
-
     
+
+ 
    
     
 }
